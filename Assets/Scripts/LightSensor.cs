ef2a3b2b6f310ba287c24f7d999cbed883139c1e
Laconiq <laconiqofficial@gmail.com>
2023-10-22 10:33:17 +0200

    Work on lightsensor

--- Assets/Scripts/LightSensor.cs
+++ Assets/Scripts/LightSensor.cs
@@ -1,7 +1,15 @@
+using MoreMountains.Feedbacks;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.U2D;
+
 public class LightSensor : MonoBehaviour
 {
     [SerializeField] private GameObject objectToEnable;
+    [SerializeField] private Light2D _light2D;
+    [SerializeField] private float minIntensity;
+    [SerializeField] private float maxIntensity;
+    private MMF_Player _mmfPlayer;
     private Animator _animator;
     private BoxCollider2D _boxCollider2D;
     private int _activeLightCount;
@@ -10,6 +18,7 @@ public class LightSensor : MonoBehaviour
     {
         _boxCollider2D = objectToEnable.GetComponent<BoxCollider2D>();
         _animator = objectToEnable.GetComponent<Animator>();
+        _mmfPlayer = GetComponent<MMF_Player>();
     }
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -25,15 +34,11 @@ public class LightSensor : MonoBehaviour
     }
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Raycast"))
-        {
-            _activeLightCount--;
-            if (_activeLightCount <= 0 && _isSensorEnabled)
-            {
-                LightSensorSwitch(false);
-                _isSensorEnabled = false;
-            }
-        }
+        if (other.gameObject.layer != LayerMask.NameToLayer("Raycast")) return;
+        _activeLightCount--;
+        if (_activeLightCount > 0 || !_isSensorEnabled) return;
+        LightSensorSwitch(false);
+        _isSensorEnabled = false;
     }
     private void LightSensorSwitch(bool isEnabled)
     {
@@ -42,12 +47,15 @@ public class LightSensor : MonoBehaviour
             _boxCollider2D.enabled = false;
             if (_animator == null) return;
             _animator.SetBool("isEnable", true);
+            _light2D.intensity = maxIntensity;
+            _mmfPlayer.PlayFeedbacks();
         }
         else
         {
             _boxCollider2D.enabled = true;
             if (_animator == null) return;
             _animator.SetBool("isEnable", false);
+            _light2D.intensity = minIntensity;
         }
     }
 }
